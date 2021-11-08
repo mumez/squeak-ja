973b76f1169ec9c6c4b255580237f7adff2cda03
Masashi Umezawa <ume@softumeya.com>
2021-11-08 21:57:02 +0900

    Adding PatchesJa20110321-5.3 sar sources

--- patches-source/5.3/PatchesJa20110321-5.3/5.3-extra-patches-new/MenuAddActionTranslate-mu.1.cs
+++ patches-source/5.3/PatchesJa20110321-5.3/5.3-extra-patches-new/MenuAddActionTranslate-mu.1.cs
@@ -1,4 +1,4 @@
-'From Squeak5.3 of 28 May 2021 [latest update: #19459] on 6 November 2021 at 10:34:56 pm'!!CustomMenu methodsFor: 'construction' stamp: 'mu 4/25/2010 00:13'!add: aString action: actionItem
+'From Squeak5.3 of 28 May 2021 [latest update: #19459] on 7 November 2021 at 10:13:33 pm'!!CustomMenu methodsFor: 'construction' stamp: 'mu 4/25/2010 00:13'!add: aString action: actionItem
 	"Add the given string as the next menu item. If it is selected, the given action (usually but not necessarily a symbol) will be returned to the client."
 
 	| s |
@@ -8,7 +8,7 @@
 	s replaceFrom: 2 to: s size - 1 with: aString.
 	s at: s size put: Character space.
 	labels addLast: s.
-	selections addLast: actionItem.! !!MenuMorph methodsFor: 'construction' stamp: 'mu 11/6/2021 22:34'!add: aString action: aSymbolOrValuable 	"Append a menu item with the given label. If the item is selected, it will send the given selector to the default target object."	"Details: Note that the menu item added captures the default target object at the time the item is added; the default target can later be changed before added additional items without affecting the targets of previously added entries. The model is that each entry is like a button that knows everything it needs to perform its action."	aSymbolOrValuable isSymbol		ifTrue:			[ self				add: aString translated				target: defaultTarget				selector: aSymbolOrValuable				argumentList: Array empty ]		ifFalse:			[ self				add: aString translated				target: aSymbolOrValuable				selector: #value				argumentList: Array empty ]! !!PluggableMenuSpec methodsFor: 'construction' stamp: 'mu 4/25/2010 00:13'!add: aString action: aMessageSend
+	selections addLast: actionItem.! !!MenuMorph methodsFor: 'construction' stamp: 'mu 11/7/2021 22:12'!add: aString target: target selector: aSymbol argumentList: argList	"Append a menu item with the given label. If the item is selected, it will send the given selector to the target object with the given arguments. If the selector takes one more argument than the number of arguments in the given list, then the triggering event is supplied as as the last argument."	| item |	item := MenuItemMorph new		contents: aString translated;		target: target;		selector: aSymbol;		arguments: argList asArray.	self addMorphBack: item.! !!PluggableMenuSpec methodsFor: 'construction' stamp: 'mu 4/25/2010 00:13'!add: aString action: aMessageSend
 	| item |
 	item := self addMenuItem.
 	item label: aString translated.
