ca22a094eb864a87bcab0dcd49845580f7d219e9
Masashi Umezawa <ume@softumeya.com>
2021-11-26 20:12:04 +0900

    Fixed JapaneseEnvironmentConverterClassFix-mu.1.cs and X11EncodingFallbackToUtf8-mu.1.cs

--- patches-source/5.3/PatchesJa20211119-5.3/5.3-extra-patches/X11EncodingFallbackToUtf8-mu.1.cs
+++ patches-source/5.3/PatchesJa20211119-5.3/5.3-extra-patches/X11EncodingFallbackToUtf8-mu.1.cs
@@ -1,1 +1,15 @@
-'From Squeak5.3 of 28 May 2021 [latest update: #19459] on 24 November 2021 at 11:59:13 pm'!"Change Set:		X11EncodingFallbackToUtf8-muDate:			24 November 2021Author:			Masashi UmezawaX11Encoding class >> encoding returns 'utf-8' as a fallback encoding when primitive call is  failed."!!JapaneseEnvironment class methodsFor: 'subclass responsibilities' stamp: 'mu 11/24/2021 23:56'!clipboardInterpreterClass	| platformName osVersion |	platformName := Smalltalk platformName.	osVersion := Smalltalk osVersion.	platformName = 'Win32' ifTrue: [^UTF8ClipboardInterpreter].	platformName = 'Mac OS' ifTrue: [^MacShiftJISClipboardInterpreter].	^platformName = 'unix' 		ifTrue: 			[(ShiftJISTextConverter encodingNames includes: X11Encoding encoding) 				ifTrue: [MacShiftJISClipboardInterpreter]				ifFalse: [UnixJPClipboardInterpreter]]		ifFalse: [ NoConversionClipboardInterpreter ]! !!X11Encoding class methodsFor: 'encoding' stamp: 'mu 11/24/2021 23:56'!encoding	| enc |	enc := self getEncoding.	enc isString ifTrue: [ ^enc asLowercase].	^'utf-8'! !
+'From Squeak5.3 of 28 May 2021 [latest update: #19459] on 24 November 2021 at 11:59:13 pm'!
+"Change Set:		X11EncodingFallbackToUtf8-mu
+Date:			24 November 2021
+Author:			Masashi Umezawa
+
+X11Encoding class >> encoding returns 'utf-8' as a fallback encoding when primitive call is  failed."!
+
+!X11Encoding class methodsFor: 'encoding' stamp: 'mu 11/24/2021 23:56'!
+encoding
+
+	| enc |
+	enc := self getEncoding.
+	enc isString ifTrue: [ ^enc asLowercase].
+	^'utf-8'! !
+
