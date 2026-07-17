7aa972ef7bc3582586f5037b4af4971766267837
Masashi Umezawa <ume@softumeya.com>
2026-07-17 22:32:07 +0900

    Adding 6.0 patches

---
+++ patches-source/6.0/PatchesJa20211111-6.0/6.0-extra-patches/DialogTitleMessageTranslated-mu.1.cs
@@ -0,0 +1,5 @@
+'From Squeak5.3 of 28 May 2021 [latest update: #19459] on 11 November 2021 at 10:05:23 pm'!"Change Set:		DialogTitleMessageTranslated-mu
+Date:			11 November 2021
+Author:			Masashi Umezawa
+
+Added #translated to Dialog title & message"!!DialogWindow methodsFor: 'accessing' stamp: 'MU 7/17/2026 21:43'!message: aStringOrText	aStringOrText size > 900 ifTrue: [		"NOTE THAT this is an awkward compromise for clients misusing the medium of a dialog window. Because of a dialog's rather short lifespan, users should only be presented with compact content and a concise message. If you notice that your dialog is getting too tall or -- in this case -- wider than usual, please consider rewriting its message."		self messageMorph averageLineLength: 65].			messageMorph contents: aStringOrText translated.	self setMessageParameters.! !!DialogWindow methodsFor: 'accessing' stamp: 'mu 11/11/2021 22:03'!title: aString	titleMorph contents: aString asText translated.	self setTitleParameters.! !
